db62c8d4e1348bdecb763ce9107c5d3fa0343d5c
gvastrad <97380294+gvastrad@users.noreply.github.com>
2023-11-28 19:34:09 +0530

    Add files via upload

---
+++ hooksdemo/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import StateHook2 from './components/StateHook2';
+function App() {
+  return (
+    <div className="App">
+      <StateHook2/>
+    </div>
+  );
+}
+
+export default App;
